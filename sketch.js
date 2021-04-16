d8928c6aa12f4b6c593c9ca0d1b9147e9e7a7e9f
kislayaswal <81315534+kislayaswal@users.noreply.github.com>
2021-04-16 21:13:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,41 @@
-var fixedRect, movingRect;
-var gameObject1, gameObject2;
+//create a namespace
+const MEngine = Matter.Engine;
+const MBodies = Matter.Bodies;
+const MWorld = Matter.World;
 
-function setup() {
-  createCanvas(1200,800);
- 
- 
-  movingRect = createSprite(800, 400,80,30);
-  movingRect.shapeColor = "green";
+var myEngine, myWorld;
+var rect1;
+var ground;
 
-  fixedRect = createSprite(100, 100, 50, 50);
-  fixedRect.shapeColor = "green";
-  
-  
+function setup(){
+  var canvas=createCanvas(400,400);
+  //create an engine
+  myEngine = MEngine.create();
+  //create a world 
+  myWorld = myEngine.world;
+  //create physic properties for yjr body
+  var opts = {
+    isStatic: false, 
+    restitution: 0.8
+
+  };
+//create a rectangluar body
+  rect1 = MBodies.rectangle(200,200,40,50,opts);
+  //added the rectangle to the world.
+  MWorld.add(myWorld,rect1);
+  console.log(rect1);
+  var opts1={
+    isStatic: true
+  };
+  ground=MBodies.rectangle(200,380,400,20,opts1);
+  MWorld.add(myWorld,ground);
 }
-
-function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-
- if(isTouching(movingRect,fixedRect)){
-    movingRect.shapeColor = "blue";
-    fixedRect.shapeColor = "blue";
-  }
-  
-  else {
-    movingRect.shapeColor = "green";
-  
-    fixedRect.shapeColor = "green"
-  }
- 
-
-//bounceOff(movingRect,fixedRect)
-  drawSprites();
+function draw()
+{
+  background(0);
+  //update our engine
+  MEngine.update(myEngine);
+  rectMode(CENTER);
+    rect(rect1.position.x,rect1.position.y,50,50);
+  rect(ground.position.x,ground.position.y,400,20)
 }
-
